4555bc0accea583028a37023febf90b3e9527d52
westermost <hlong1606@gmail.com>
2020-08-12 23:20:43 +0700

    End Section 5

--- Book_LongLH9.DataAccess/Data/ApplicationDbContext.cs
+++ Book_LongLH9.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Book_LongLH9.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace Book_LongLH9.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
