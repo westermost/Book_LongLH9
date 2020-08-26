e5dad2d0f9b7e009ac007f6d572ce17e68f5109f
westermost <hlong1606@gmail.com>
2020-08-26 21:44:23 +0700

    add Repository

---
+++ Book_LongLH9.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_LongLH9.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T :class
+    {
+    }
+}
